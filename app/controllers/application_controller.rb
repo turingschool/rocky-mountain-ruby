e5595056b3a00672fc1d541f2cfa15a9ff5ebf50
Jeff Casimir <jeff@casimircreative.com>
2017-09-22 08:48:00 -0600

    Add Brittany title

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,7 +20,7 @@ class ApplicationController < ActionController::Base
       {time: '1:55 pm',  length: '35 minutes', title: 'Adam Cuppy'},
   		{time: '2:35 pm',  length: '10 minutes', title: '<em>Community Spotlight</em>: Elaine Marino from Equili'},
       {time: '2:50 pm',  length: '35 minutes', title: 'Vaidehi Joshi'},
-      {time: '3:30 pm',  length: '35 minutes', title: 'Brittany Storoz'},
+      {time: '3:30 pm',  length: '35 minutes', title: 'Brittany Storoz: <em>Comparative Error Handling: Learning by Making All the Mistakes</em>'},
       {time: '4:10 pm',  length: '35 minutes', title: 'Ben Orenstein'},
       {time: '4:45 pm',  length: '5 minutes',  title: 'Thanks & Closing'},
       {time: '4:50 pm',  length: '85 minutes', title: 'Social Hour'}
